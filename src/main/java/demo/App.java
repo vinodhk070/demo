f8593440dd466a50a75f405f415429ab97cb9931
vinodhk070 <vinodhk070@gmail.com>
2020-05-14 15:45:05 +0530

    app.java saved

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -4,10 +4,10 @@ package demo;
  * Hello world!
  *
  */
-public class App 
+publi class App 
 {
     public static void main( String[] args )
-    {
+    
         System.out.println( "Hello World!" );
     }
 }
